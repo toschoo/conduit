185f266871c4376257d73d0cfc9044694aa74bc0
Tobias Schoofs <tobias.schoofs@gmx.net>
2017-03-25 19:55:55 +0000

    initial commit

---
+++ conduit.go
@@ -0,0 +1,175 @@
+// Package conduit provides abstractions over computation chains
+// similar to classic coroutines and inspired mostly by Haskell's
+// conduit library.
+// Its main abstractions are chains consisting of a data producer,
+// a data consumer and, potentially, a pipe of conduits transforming
+// or filtering data on their way down the processing chain.
+// Each component of a chain, producer, consumer and each conduit,
+// is running in its own goroutine receiving and forwarding
+// data through a channel. Chains are therefore not only a way
+// for separation of concerns in code design, but also a way to speed up
+// processing exploiting multicore architectures.
+// 
+// The position of a component in the chain determines
+// the input data that it can expect and the output data it may have
+// to send. Since the data passed on in the chain are modelled
+// as interface{}, any data qualifies from the compiler perspective.
+// A receiver (conduit or consumer) needs to perform a type assertion
+// to safely convert the incoming data to the expected type.
+// This may fail. Application developers, therefore, need to pay 
+// special attention to data types used in concrete implementations.
+package conduit
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+// Source is an input channel
+type Source <-chan interface{}
+
+// Target is an output channel
+type Target chan<- interface{}
+
+// Producer creates data and sends them
+// down the processing chain.
+type Producer interface {
+	Produce(trg Target) error
+}
+
+// Consumer is the endpoint of the processing chain.
+type Consumer interface {
+	Consume(src Source) error
+}
+
+// Conduit sits in the middle of a processing chain
+// receiving data, processing them in some form and sending
+// them further down.
+type Conduit interface {
+	Conduct(src Source, trg Target) error
+}
+
+// Chain encapsulates the chain processing
+// and hides anything irrelevant for users
+// building applications.
+// Errors that led to the termination of one
+// or more components can be inspected through Errs.
+type Chain struct {
+	door  sync.Mutex
+	sz    uint32 // buffer size
+	e     bool   // there were errors
+	p     Producer
+	c     Consumer
+	pipe  []Conduit
+	Errs  []error
+}
+
+// Resets the chain for a new round of processing.
+func (ch *Chain) reset() {
+	ch.Errs = make([]error, 8)
+	ch.e = false
+}
+
+// Adds an error to the processing chain.
+func (ch *Chain) addErr(err error) {
+
+	ch.door.Lock()
+	defer ch.door.Unlock()
+
+	if !ch.e {
+		ch.e = true
+	}
+	ch.Errs = append(ch.Errs, err)
+}
+
+// Runs one conduit
+func (ch *Chain) pipe2pipe(src Source, trg Target, p Conduit) {
+	defer close(trg)
+	err := p.Conduct(src, trg)
+	if err != nil {
+		ch.addErr(err)
+	}
+}
+
+// Starts all conduits
+func (ch *Chain) runPipe(c0 chan interface{}) (ret chan interface{}, err error) {
+	ret = c0
+	src := c0
+
+	for _, p := range ch.pipe {
+		trg := make(chan interface{}, ch.sz)
+		if trg == nil {
+			s := fmt.Sprintf("cannot create channel\n")
+			err = errors.New(s)
+			break
+		}
+		go ch.pipe2pipe(src, trg, p)
+		src, ret = trg, trg
+	}
+	return
+}
+
+// Run starts the chain.
+// If the process was interrupted,
+// Run terminates with an error.
+// Errors that were reported by faulty components
+// are written to Errs and can be inspected afterwards.
+func (ch *Chain) Run() error {
+
+	ch.reset()
+
+	c1 := make(chan interface{}, ch.sz)
+	if c1 == nil {
+		s := fmt.Sprintf("cannot create channel\n")
+		return errors.New(s)
+	}
+
+	c2, err := ch.runPipe(c1)
+	if err != nil {
+		s := fmt.Sprintf("cannot run pipe: %v\n", err)
+		return errors.New(s)
+	}
+
+	go func() {
+		defer close(c1)
+		perr := ch.p.Produce(c1)
+		if perr != nil {
+			ch.addErr(perr)
+		}
+	}()
+
+	cerr := ch.c.Consume(c2)
+	if cerr != nil {
+		ch.addErr(cerr)
+	}
+	if (ch.e) {
+		return errors.New("Errors occurred")
+	}
+	return nil
+}
+
+// NewChain creates a new chain.
+// The method expects a producer and a consumer (both mandatory),
+// a pipe of Conduits (which may be nil) and a parameter indicating
+// the buffer size of channels.
+// Note that the order of conduits in the pipe
+// determines the order in which they are chained together and processed.
+func NewChain(p Producer, pipe []Conduit, c Consumer, sz uint32) (ch *Chain) {
+	if p == nil || c == nil {
+		return nil
+	}
+	ch = new(Chain)
+	if ch != nil {
+		ch.p = p
+		ch.c = c
+		ch.pipe = pipe
+		ch.sz = sz
+		ch.e = false
+		ch.Errs = make([]error, 8)
+		if ch.Errs == nil {
+			return nil
+		}
+	}
+	return
+}
---
+++ utils/utils.go
@@ -0,0 +1,288 @@
+// Package utils provides a collection of
+// useful and instructive producers, consumers 
+// and conduits.
+package utils
+
+import (
+	"github.com/toschoo/golang/conduit"
+	"encoding/csv"
+	"fmt"
+	"io"
+)
+
+// Generators are expected to provide an interface
+// to generate data that are then fed into the processing chain.
+// The Generic Producer uses a user-defined Generator
+// to create data.
+type Generator interface {
+	Generate() (interface{}, error)
+}
+
+// Generic is a Producer that uses a Generator
+// to create data. It continues calling Generate()
+// and sending the result down the chain, util
+// Generate() returns io.EOF.
+type Generic struct {
+	gen Generator
+}
+
+// Produce is the pre-defined method that
+// makes Generic a Producer.
+func (g *Generic) Produce(trg conduit.Target) error {
+	for {
+		rec, err := g.gen.Generate()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		trg <- rec
+	}
+	return nil
+}
+
+// NewGeneric creates a new Generic Producer
+// using a user-defined Generator.
+func NewGeneric(gen Generator) (g *Generic) {
+	g = new(Generic)
+	if g != nil {
+		g.gen = gen
+	}
+	return
+}
+
+// Reader is a Producer that feeds data
+// read from some kind of source 
+// into the processing chain.
+type Reader struct {
+	rd   io.Reader
+	sz   int
+	text bool
+}
+
+// Produce is the pre-defined method that
+// makes Reader a Producer.
+func (rd *Reader) Produce(trg conduit.Target) error {
+	buf := make([]byte,rd.sz)
+	for {
+		n, err := rd.rd.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		if rd.text {
+			trg <- string(buf[:n])
+		} else {
+			trg <- buf[:n]
+		}
+	}
+	return nil
+}
+
+// NewByteReader creates a new Reader Producer
+// using some kind of io.Reader.
+// The data is sent down the chain as []byte.
+func NewByteReader(reader io.Reader) (r *Reader) {
+	r = new(Reader)
+	if r != nil {
+		r.rd = reader
+		r.sz = 8192
+		r.text = false
+	}
+	return
+}
+
+// NewTextReader creates a new Reader Producer
+// using some kind of io.Reader.
+// The data is sent down the chain as string.
+func NewTextReader(reader io.Reader) (r *Reader) {
+	r = NewByteReader(reader)
+	if r != nil {
+		r.text = true
+	}
+	return
+}
+
+// CSV is a Producer that feeds data
+// read from a CSV-encoded source 
+// line by line into the processing chain.
+// CSV releases data as string slices,
+// each slice representing 
+// one line in the CSV source.
+type CSV struct {
+	Rd *csv.Reader
+}
+
+// Produce is the pre-defined method that
+// makes CSV a Producer.
+func (p *CSV) Produce(trg conduit.Target) error {
+	for {
+		rec, err := p.Rd.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+		trg <- rec
+	}
+	return nil
+}
+
+// NewCSV creates a new CSV Producer
+// using some kind of io.Reader.
+func NewCSV (r io.Reader) (p *CSV) {
+	p = new(CSV)
+	if p != nil {
+		p.Rd = csv.NewReader(r)
+	}
+	return
+}
+
+// Identity is a Conduit that 
+// forwards incoming data as is.
+// It is useful only as a demonstration
+// and testing device. 
+type Identity struct{}
+
+// Conduct is the predefined method that makes Identity a Conduit.
+func (id *Identity) Conduct(src conduit.Source, trg conduit.Target) error {
+	for i := range src {
+		trg <- i
+	}
+	return nil
+}
+
+// Creates a new Identity.
+func NewIdentity() (id *Identity) {
+	id = new(Identity)
+	return
+}
+
+// Sieves are expected to provide an interface
+// to filter incoming data. Only those data that
+// pass the filter are then sent down the chain.
+// The Filter Conduit uses a user-defined Sieve
+// to filter data.
+type Sieve interface {
+	Sieve(interface{}) bool
+}
+
+// Filter is a Conduit that 
+// forwards incoming data based on a Sieve.
+// Only those data are passed onward that
+// pass the filter.
+type Filter struct {
+	f Sieve
+}
+
+// Conduct is the predefined method that makes Filter a Conduit.
+func (fil *Filter) Conduct(src conduit.Source, trg conduit.Target) error {
+	for inp := range src {
+		if fil.f.Sieve(inp) {
+			trg <- inp
+		}
+	}
+	return nil
+}
+
+// NewFilter creates a new Filter based on a user-defined Sieve.
+func NewFilter(filter Sieve) (fil *Filter) {
+	fil = new(Filter)
+	if fil != nil {
+		fil.f = filter
+	}
+	return
+}
+
+// Transforms are expected to provide the Transform method
+// that transforms incoming data and sends the result
+// down the processing chain. Transforms are used by
+// Transformers to implement generic transformation conduits.
+type Transform interface {
+	Transform(interface{}) (interface{}, error)
+}
+
+// Transformer is a conduit that uses a Transform
+// to process incoming data. It passes the result
+// onward in the processing chain.
+// Note that, when Transform returns nil as result, 
+// this specific item is skipped
+// and processing continues with the next item.
+// Transformers, hence, can be used to implement filters.
+type Transformer struct {
+	t Transform
+}
+
+// Conduct is the pre-defined method that makes Transformer a Conduit.
+func (trn *Transformer) Conduct(src conduit.Source, trg conduit.Target) error {
+	for inp := range src {
+		oup, err := trn.t.Transform(inp)
+		if err != nil {
+			return err
+		}
+		if oup == nil {
+			continue
+		}
+		trg <- oup
+	}
+	return nil
+}
+
+// NewTransformer creates a new Transformer
+// based on a user-defined Transform.
+func NewTransformer(trnf Transform) (trn *Transformer) {
+	trn = new(Transformer)
+	if trn != nil {
+		trn.t = trnf
+	}
+	return
+}
+
+// Printer is a Consumer that writes the result
+// as text to some kind of io.Writer using fmt.Fprintf.
+type Printer struct {
+	stream io.Writer
+	text   bool
+}
+
+// Consume is the pre-defined method 
+// that makes Printer a Consumer.
+func (prn *Printer) Consume(src conduit.Source) error {
+	for inp := range src {
+		if prn.text {
+			buf := inp.([]byte)
+			runes := string(buf)
+			fmt.Fprintf(prn.stream, "%s\n", runes)
+		} else {
+			fmt.Fprintf(prn.stream, "%v\n", inp)
+		}
+	}
+	return nil
+}
+
+// NewPrinter creates a new Printer Consumer
+// that writes the data using fmt.Fprintf with verb "%v".
+func NewPrinter(stream io.Writer) (p *Printer) {
+	p = new(Printer)
+	if p != nil {
+		p.stream = stream
+		p.text   = false
+	}
+	return
+}
+
+// NewTextPrinter creates a new Printer Consumer
+// that writes the data using fmt.Fprintf with ver %s.
+// TextPrinter expects incoming data to be byte slices, 
+// which are explictly converted to strings.
+func NewTextPrinter(stream io.Writer) (p *Printer) {
+	p = NewPrinter(stream)
+	if p != nil {
+		p.text = true
+	}
+	return
+}
